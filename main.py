4ab8b77f9adc2984344216dfc7d0bdf28fcaedd5
KriszD <kriszemails@gmail.com>
2025-03-23 21:56:24 -0400

    created average_bacon_number method to compute ALL bacon numbers

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 """Running the program"""
 from graph_entities import Graph
+import graph_create
 
 
 def bacon_path(graph: Graph, actor1: str, actor2: str) -> tuple[list, list]:
     """Given the name of two actors, find the path between them, return two lists, one with movies and one without"""
+    actors = graph.get_all_vertices('actor')
+    if actor1 not in actors or actor2 not in actors:
+        print("At least one of those actors is not in this graph.")
+        raise ValueError
     path = graph.shortest_path(actor1, actor2)
     path_with_movies = []
 
@@ -61,3 +66,14 @@ def average_bacon_number(graph: Graph, actor: str) -> int:
         total_bacon_numbers += 1
 
     return sum_bacon_numbers // total_bacon_numbers
+
+
+def compute_average_bacon_number(graph_name: str) -> dict:
+    """Compute a full dictionary of actors and their average bacon_numbers for a given dataset."""
+    graph, _ = graph_create.initialize_graphs(graph_name)
+    actors = graph.get_all_vertices('actor')
+    bacon_numbers = {}
+    for actor in actors:
+        bacon_numbers[actor] = average_bacon_number(graph, actor)
+
+    return bacon_numbers
